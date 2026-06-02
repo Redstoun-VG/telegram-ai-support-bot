fe58c3d9a9f232a448eaae4a227f59b650da7e89
Vlad <facepalmerqas@mail.ru>
2026-06-02 17:44:37 +0300

    add branding assets

--- handlers/start.py
+++ handlers/start.py
@@ -3,18 +3,29 @@ from email import message
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.types import FSInputFile
 
 from keyboards import main_keyboard
 
 router = Router()
 
+
+
 @router.message(CommandStart())
 async def start(message: Message):
 
 
-    await message.answer(
+    photo = FSInputFile(
+    "images/welcome.png"
+)
+
+    await message.answer_photo(
+    photo=photo,
+    caption=
     "🤖 Добро пожаловать в AI Support Bot!\n\n"
-    "Здесь вы сможете получать ответы на вопросы и поддержку.",
+    "Ваш умный помощник всегда на связи 🚀",
     reply_markup=main_keyboard
 )
+    
+    
 
